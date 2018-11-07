8c91f69f0a59906893cbd58a9f40a50dc1b55609
Dmitry <Scarecrow365@github.com>
2018-11-07 19:10:31 +0300

    TestProject

---
+++ TestProject/Assets/TestProject/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    private Rigidbody _rb;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        _rb.AddForce(Vector3.down * _speed);
+    }
+}
