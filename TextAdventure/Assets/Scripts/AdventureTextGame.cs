8f1d93df01bd8581a03adf23aa76c336a2c2b972
Dmitry <Scarecrow365@github.com>
2019-08-13 11:53:21 +0300

    New project

---
+++ TextAdventure/Assets/Scripts/AdventureTextGame.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace TextAdventureGame.Scripts
+{
+    public class AdventureTextGame : MonoBehaviour
+    {
+        private State _state;
+        [SerializeField] private State startingState;
+        [SerializeField] private TextMeshProUGUI textComponent;
+        [SerializeField] private TextMeshProUGUI TitleText;
+
+        private void Start()
+        {
+            _state = startingState;
+            textComponent.text = _state.GetStateStory();
+        }
+
+        private void Update()
+        {
+            ManageState();
+        }
+
+        private void ManageState()
+        {
+            var nextStates = _state.GetNextStates();
+
+            for (var i = 0; i < nextStates.Length; i++)
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                    _state = nextStates[i];
+            textComponent.text = _state.GetStateStory();
+            TitleText.text = _state.name;
+        }
+    }
+}
---
+++ TextAdventure/Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace TextAdventureGame.Scripts
+{
+    [CreateAssetMenu(menuName = "State")]
+    public class State : ScriptableObject
+    {
+        [SerializeField] private State[] nextStates;
+        [TextArea(14, 10)] [SerializeField] private string storyText;
+
+        public string GetStateStory()
+        {
+            return storyText;
+        }
+
+        public State[] GetNextStates()
+        {
+            return nextStates;
+        }
+    }
+}
