29a349d7dc00665474a3d9792d78684ffcf194bf
Dmitry <Scarecrow365@github.com>
2019-09-20 19:51:22 +0300

    Space Invaders

---
+++ Space Invaders/Assets/Scripts/Level.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    private int _enemyShips;
+    private SceneLoader _sceneLoader;
+
+    private void Start()
+    {
+        _sceneLoader = GetComponent<SceneLoader>();
+    }
+
+    public void CountShips()
+    {
+        _enemyShips++;
+    }
+
+    public int CountAllShips()
+    {
+        return _enemyShips;
+    }
+
+    public void DestroyShip()
+    {
+        _enemyShips--;
+
+        if (_enemyShips <= 0)
+            _sceneLoader.ReloadScene();
+    }
+}
